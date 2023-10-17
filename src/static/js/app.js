7e0a57159f21674d043615fb5c1b890f5a69c75a
Emil <92442806+emilroy4@users.noreply.github.com>
2023-10-17 15:43:17 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,3 +1,5 @@
+
+
 function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
@@ -177,3 +179,22 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
