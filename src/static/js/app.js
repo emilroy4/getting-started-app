8574610498171b0feb70a88d296e8381b9cee4b6
Emil <92442806+emilroy4@users.noreply.github.com>
2023-10-17 20:08:41 +0100

    Update app.js

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -207,17 +207,17 @@ let temp = document.querySelector('.temp')
 let desc = document.querySelector('.desc')
 
 //adding event listener to search button
-button1.addEventListener('click', function(){
-	//Fetch data from api
-	fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=e32cac0d7841173d064cf59b12aadebd')
-	.then(response => response.json())
-	.then(
-		displayData)
-	.catch(err => alert('Wrong city name'));
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=108dd9a67c96f23039937fe6f3c91963`)
+    .then(response => response.json())
+    .then(
+        displayData)
+    .catch(err => alert('Wrong City name')); 
+
 })
 //function to display
 const displayData=(weather)=>{
-	temp.innerText=`${weather.main.temp}*C`
-	desc.innerText=`${weather.weather[0].main}`
+    temp.innerText=`${weather.main.temp}°C`
+    desc.innerText=`${weather.weather[0].main}`
 }
 
