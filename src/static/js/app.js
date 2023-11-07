075e1533c9b3bdb1c7c19ac850f5e8aab299019b
Emil <92442806+emilroy4@users.noreply.github.com>
2023-11-07 20:57:21 +0000

    button visible

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -44,6 +44,7 @@ function TodoListCard() {
 
     return (
         <React.Fragment>
+            <AddItemForm />
             {items.length === 0 && (
                 <p className="text-center">No items yet! Add one above!</p>
             )}
@@ -56,6 +57,46 @@ function TodoListCard() {
                 />
             ))}
         </React.Fragment>
+    );
+}
+
+function AddItemForm() {
+    const { Form, InputGroup, Button } = ReactBootstrap;
+
+    const [newItem, setNewItem] = React.useState('');
+    const [submitting, setSubmitting] = React.useState(false);
+
+    const submitNewItem = e => {
+        e.preventDefault();
+        setSubmitting(true);
+        // Instead of making a request to add an item, you can simply clear the form.
+        setNewItem('');
+        setSubmitting(false);
+    };
+
+    return (
+        <Form onSubmit={submitNewItem}>
+            <InputGroup className="mb-3">
+                <Form.Control
+                    value={newItem}
+                    onChange={e => setNewItem(e.target.value)}
+                    type="text"
+                    placeholder="New Item"
+                    aria-describedby="basic-addon1"
+                    disabled={submitting}  // Disable the input field when submitting
+                />
+                <InputGroup.Append>
+                    <Button
+                        type="submit"
+                        variant="success"
+                        disabled={!newItem.length || submitting}  // Disable the button when submitting
+                        className={submitting ? 'disabled' : ''}
+                    >
+                        {submitting ? 'Adding...' : 'Add'}
+                    </Button>
+                </InputGroup.Append>
+            </InputGroup>
+        </Form>
     );
 }
 
@@ -122,79 +163,3 @@ function ItemDisplay({ item, onItemUpdate, onItemRemoval }) {
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-//title
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml6 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml6 .letter',
-    translateY: ["1.1em", 0],
-    translateZ: 0,
-    duration: 750,
-    delay: (el, i) => 50 * i
-  }).add({
-    targets: '.ml6',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-
-//weather title
-// Wrap every letter in a span
-var textWrapper = document.querySelector('.ml14 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml14 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeInOutExpo",
-    duration: 900
-  }).add({
-    targets: '.ml14 .letter',
-    opacity: [0,1],
-    translateX: [40,0],
-    translateZ: 0,
-    scaleX: [0.3, 1],
-    easing: "easeOutExpo",
-    duration: 800,
-    offset: '-=600',
-    delay: (el, i) => 150 + 25 * i
-  }).add({
-    targets: '.ml14',
-    opacity: 0,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-
-//weather
-// ACCESSING ALL THE HTML COMPONENTS REQUIRED TO PERFORM ACTIONS ON.
-let button = document.querySelector('.button')
-let inputvalue = document.querySelector('.inputValue')
-let nameVal = document.querySelector('.name');
-let temp = document.querySelector('.temp');
-let desc = document.querySelector('.desc');
-
-
-// ADDING EVENT LISTENER TO SEARCH BUTTON  
-button.addEventListener('click', function(){
-
-    // Fection data from open weather API
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputvalue.value}&units=metric&appid=e32cac0d7841173d064cf59b12aadebd`)
-    .then(response => response.json())
-    .then(displayData)
-    .catch(err => alert('Wrong City name')); 
-
-})
-
-// Function to display weather on the HTML document
-const displayData = (weather) => {
-    temp.innerText = `${weather.main.temp}°C`
-    desc.innerText = `${weather.weather[0].main}`
-}
